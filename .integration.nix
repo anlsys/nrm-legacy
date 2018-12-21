37f5594287f77e42f3bf5f6b5a89f387c8905137
Valentin Reis <valentin.reis@gmail.com>
2018-12-21 08:55:30 -0600

    [ci] Updated integration tests.
    These use libnrm/downstream-refactor and should be updated once that
    branch is merged.

--- .integration.nix
+++ .integration.nix
@@ -1,8 +1,12 @@
 { argotest ? (builtins.fetchGit {
     url = https://xgitlab.cels.anl.gov/argo/argotest.git;
-    ref="refactor-argotk";
-    rev="a20358e5b72f267eb8e2a9152e62c9ebbb3b2d4a"; })
+    ref="master";
+    rev="646d42f7b64f56cdb3ff54a7b4a59e0dfad3209c";
+})
 }:
 (import argotest {
   nrm-src = ./.;
+  libnrm-src = builtins.fetchGit {
+      url = https://xgitlab.cels.anl.gov/argo/libnrm.git;
+      ref="downstream-refactor"; };
 }).test
