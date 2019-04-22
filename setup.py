a602b4644f848ea1fd7fe7cf6a46a2915ba3baa6
Valentin Reis <vreis@anl.gov>
2019-04-22 15:53:17 -0500

    [refactor] renaming command-line utilities
    cmd -> nrm
    daemon -> nrmd
    argo-perf-wrapper -> nrm-perfwrapper

--- setup.py
+++ setup.py
@@ -21,5 +21,5 @@ setup(
 
     packages=find_packages(),
     install_requires=[],
-    scripts=['bin/daemon', 'bin/app', 'bin/cmd', 'bin/argo-perf-wrapper']
+    scripts=['bin/nrmd', 'bin/app', 'bin/nrm', 'bin/nrm-perfwrapper']
 )
